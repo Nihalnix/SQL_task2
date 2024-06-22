5d944638629d2e5e9f58031cee1cfbeefe45711f
Nihalnix <nihalnix47@gmail.com>
2024-06-22 23:46:18 +0530

    SQL_task2

---
+++ SQL_task 2.sql
@@ -0,0 +1,33 @@
+select * from netflix
+
+select high from netflix
+
+select low from netflix
+
+select close from netflix
+
+select volume from netflix
+
+select date, low, adj_close from netflix
+
+
+select * from netflix where close=254.259995
+
+
+select * from netflix where volume>8956705
+
+select * from netflix where low<350
+
+
+select * from netflix where low<350 or high>280
+
+select * from netflix where open>300 or adj_close<300
+
+
+
+
+
+
+
+
+
